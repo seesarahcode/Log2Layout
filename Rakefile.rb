97ade315d7fb9f5748f022cb9cb66be07dc5cfdf
sarahcwheeler <sarahcwheeler@gmail.com>
2014-03-11 23:52:00 -0700

    Added Gemfile.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,17 +1,5 @@
-require 'rake'
-require "rubygems"
-require "bundler/gem_tasks"
-Bundler.setup(:default, :test)
-  
- task :spec do
-   begin
-     require 'rspec/core/rake_task'
- 
-     desc "Run the specs under spec/"
-     RSpec::Core::RakeTask.new do |t|
-       t.spec_files = FileList['spec/**/*_spec.rb']
-     end
-   rescue NameError, LoadError => e
-     puts e
-   end
- end
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new('spec')
+
+task :default => :spec
