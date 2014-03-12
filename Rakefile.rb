6b2870442e7dd3a853338057d6879e9581944fd7
sarahcwheeler <sarahcwheeler@gmail.com>
2014-03-11 23:26:47 -0700

    Trying to fix travis tests.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,17 @@
-require 'rspec/core/rake_task'
-
-RSpec::Core::RakeTask.new('spec')
-
-task :default => :spec
+require 'rake'
+require "rubygems"
+require "bundler/gem_tasks"
+Bundler.setup(:default, :test)
+  
+ task :spec do
+   begin
+     require 'rspec/core/rake_task'
+ 
+     desc "Run the specs under spec/"
+     RSpec::Core::RakeTask.new do |t|
+       t.spec_files = FileList['spec/**/*_spec.rb']
+     end
+   rescue NameError, LoadError => e
+     puts e
+   end
+ end
